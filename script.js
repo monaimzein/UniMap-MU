2648926b38ea38bd018a83b65e4594a5670df6ea
monaimzein <monaimzein@gmail.com>
2024-06-19 17:18:42 +0300

    deploy

---
+++ script.js
@@ -0,0 +1,20 @@
+var typed = new Typed(".typing", {
+  strings: [
+    "A modern touch",
+    "for a modern university.",
+    " ",
+    "Your university, simplified.",
+    " ",
+    "Building a better campus,",
+    "together.",
+    " ",
+    "Your journey starts here.",
+    " ",
+    "It's not just a map,",
+    "it's your university compass.",
+    " ",
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+});
